49af90cbfe70f985e940958b9c706d48b9c4f25d
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-07-05 21:56:42 +0530

    fething posts from graphcms

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 /* eslint-disable @next/next/no-css-tags */
 import Head from "next/head";
 import { PostCard, PostWidget, Categories } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  { title: "Mern stack", excerpt: "Learn full stack developement" },
-  { title: "React", excerpt: "Learn frontend development" },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="contaienr mx-auto px-10 mb-8">
       <Head>
@@ -17,7 +13,7 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1 ">
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -29,4 +25,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
 }
