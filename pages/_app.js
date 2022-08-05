beaa102568aa4c9cca59fa11f27bc589bf3a8d54
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-08-06 03:09:51 +0530

    favicon added

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
+/* eslint-disable @next/next/no-css-tags */
+import Head from "next/head";
 import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <>
+      <Head>
+        <link rel="stylesheet" href="/favicon.io" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    </>
   );
 }
 
